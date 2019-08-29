fec20511654200287067a2be2ec070cef7d795cc
Shubhad30 <53821288+Shubhad30@users.noreply.github.com>
2019-08-29 12:03:20 +0530

    Add files via upload

---
+++ sdmpgm2b.py
@@ -0,0 +1,14 @@
+def func(l1):
+    l2=l1
+    l1.reverse()
+    for i in l1:
+        print(i,end=" ")
+    l2.reverse()
+    print()
+    for i in l2:
+        print(i[-1::-1],end=" ")
+s=input("Enter the string:")
+l3=s.split()
+func(l3)
+    
+        
