c9c2edd5fd285f89804ff7db1db32a9101b3bdce
Shubhad30 <53821288+Shubhad30@users.noreply.github.com>
2019-09-05 12:08:49 +0530

    Delete sdmpgm3b.py

--- sdmpgm3b.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-def func1():
-    chars=string.ascii_letters+string.digits+'!@#$%^&*'
-    size=random.randint(8,12)
-    return "".join(random.choice(chars) for i in range(size))
-print("Password:",func1())
